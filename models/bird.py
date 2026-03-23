5ae818b5ae65a0252dd5b8272b71f7cee4b86e4b
LACHIE KEANE <lachlankeane014@gmail.com>
2026-03-23 15:02:00 +0100

    Deceleration on birds flying in

--- models/bird.py
+++ models/bird.py
@@ -1,41 +1,59 @@
+import math
+
 import pygame as pg
 import globals
 import random
 from pygame.constants import RLEACCEL
+import numpy as np
 
 from entity import Entity
 
 
 class Bird(Entity):
 
-    FLY_SPEED = 4
-    LANDING_MARGIN = 0.0
+    FLY_SPEED = 6
+    DIST_BEFORE_DECEL = 100
+    DECELERATION = (FLY_SPEED**2) / (2 * DIST_BEFORE_DECEL)
+
+    LANDING_MARGIN = 0.025
     LANDING_BOUNDS = pg.Rect(globals.SCREEN_WIDTH * LANDING_MARGIN, globals.SCREEN_HEIGHT * LANDING_MARGIN,
-                             globals.SCREEN_WIDTH * (1 - LANDING_MARGIN), globals.SCREEN_HEIGHT * (1 - LANDING_MARGIN))
+                             globals.SCREEN_WIDTH * (1 - LANDING_MARGIN * 2), globals.SCREEN_HEIGHT * (1 - LANDING_MARGIN * 2))
+
+    @staticmethod
+    def get_start_pos(target):
+        closest_edge = np.argmin((target.x, globals.SCREEN_WIDTH - target.x,
+                                  target.y, globals.SCREEN_HEIGHT - target.y))
+
+        dist_from_edge = 50
+        match closest_edge:
+            case 0:
+                start_pos = pg.math.Vector2(
+                    -dist_from_edge,
+                    sorted((0, target.y + random.randint(-100, 100), globals.SCREEN_HEIGHT))[1])
+            case 1:
+                start_pos = pg.math.Vector2(
+                    globals.SCREEN_WIDTH + dist_from_edge,
+                    sorted((0, target.y + random.randint(-100, 100), globals.SCREEN_HEIGHT))[1])
+            case 2:
+                start_pos = pg.math.Vector2(
+                    sorted((0, target.x + random.randint(-100, 100), globals.SCREEN_WIDTH))[1],
+                    -dist_from_edge)
+            case 3:
+                start_pos = pg.math.Vector2(
+                    sorted((0, target.x + random.randint(-100, 100), globals.SCREEN_WIDTH))[1],
+                    globals.SCREEN_HEIGHT + dist_from_edge)
+
+        print(f'Edge: {closest_edge}')
+        print(f'Pos: {start_pos}')
+        return start_pos
 
     @staticmethod
     def random_spawn():
-        start_pos = pg.math.Vector2(0,0)
-        dist_from_edge = random.randint(50, 200)
+        target_pos = pg.math.Vector2(
+            random.randint(Bird.LANDING_BOUNDS.left, Bird.LANDING_BOUNDS.right),
+            random.randint(Bird.LANDING_BOUNDS.top, Bird.LANDING_BOUNDS.bottom))
 
-        side = random.randint(0, 4) # 0: Left, 1: Right, 2: Top, 3: Bottom
-        if side == 4: side = 0
-
-        match side:
-            case 0:
-                start_pos = pg.math.Vector2(-dist_from_edge, random.randint(0, globals.SCREEN_HEIGHT))
-            case 1:
-                start_pos = pg.math.Vector2(globals.SCREEN_WIDTH + dist_from_edge, random.randint(0, globals.SCREEN_HEIGHT))
-            case 2:
-                start_pos = pg.math.Vector2(random.randint(0, globals.SCREEN_WIDTH), -dist_from_edge)
-            case 3:
-                start_pos = pg.math.Vector2(random.randint(0, globals.SCREEN_WIDTH), globals.SCREEN_HEIGHT + dist_from_edge)
-
-
-        target_pos = pg.math.Vector2(random.randint(Bird.LANDING_BOUNDS.left, Bird.LANDING_BOUNDS.right),
-                                     random.randint(Bird.LANDING_BOUNDS.top, Bird.LANDING_BOUNDS.bottom))
-
-        return start_pos, target_pos
+        return Bird.get_start_pos(target_pos), target_pos
 
     def __init__(self):
         self.sprite = pg.image.load("content/crow.png").convert()
@@ -50,12 +68,23 @@ class Bird(Entity):
 
         # Flag for conditional movement
         self.scared = False
+        self.decelerating = False
 
     def update(self):
+
+        dist_to_target_sqrd = (self.target_pos - self.pos).length_squared()
+
         if self.vel != 0:
-            if (self.target_pos - self.pos).length_squared() < self.vel.length_squared() and not self.scared:
+            if dist_to_target_sqrd < self.vel.length_squared() and not self.scared:
                 self.pos = self.target_pos
                 self.vel = 0
+            elif not self.scared and (self.decelerating or math.sqrt(dist_to_target_sqrd) < Bird.DIST_BEFORE_DECEL):
+                self.decelerating = True
+                speed = self.vel.length() - Bird.DECELERATION
+                if speed < 0: self.vel = 0
+                else:
+                    self.vel = self.vel.normalize() * speed
+                    self.pos += self.vel
             else:
                 self.pos += self.vel
 
